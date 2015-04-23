32278f71f63bfd36fdb8e9a202438c59fe037931
Joel Rolli <joel.rolli@gmail.com>
2015-04-23 16:16:20 +0200

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       proxies: [
@@ -150,7 +150,7 @@ module.exports = function (grunt) {
       options: {
         port: 8000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '127.0.0.1',
+        hostname: '0.0.0.0',
         router: 'api/index.php'
       },
       server: {
